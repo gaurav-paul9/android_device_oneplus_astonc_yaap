90d49b33047d4d46440f7cef22b4c53ef60d2151
Arian <arian.kulmer@web.de>
2024-06-08 22:14:30 +0530

    astonc: Provide a static lib for power mode extension
    Change-Id: If4ec8af86d00cc4f7c0bd2e86e42c343b9e354fb
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 $(call soong_config_set,qtipower,mode_ext_lib,power-ext-oplus)
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-astonc)
 
 # Sensors
 PRODUCT_PACKAGES += \
