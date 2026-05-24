gaurav-paul9/android_device_oneplus_aston

Initial YAAP Bringup

AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_aston.mk
+    $(LOCAL_DIR)/yaap_aston.mk

gaurav-paul9/android_device_oneplus_astonc_yaap

AndroidProducts.mk
@@ -4,4 +4,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_astonc.mk
+    $(LOCAL_DIR)/yaap_astonc.mk